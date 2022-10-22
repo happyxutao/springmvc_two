ec1c59abd192cde97dc5eaf63726d5a90e353e97
xutao <1126556181@qq.com>
2022-10-22 17:16:03 +0800

    commit 4

--- src/main/java/javaHello.java
+++ src/main/java/javaHello.java
@@ -2,5 +2,6 @@ public class javaHello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("jlksjfs");
+        System.out.println("你好");
     }
 }
